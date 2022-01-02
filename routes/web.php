34bc6bc507b90eb665cd4e7b579386f6648c4adc
eumatv <miliandroxd@gmail.com>
2022-01-02 17:48:54 -0300

    added routes and better readability

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', fn () => inertia('Welcome', [
+    'laravelVersion' => Application::VERSION,
+    'phpVersion' => PHP_VERSION,
+]))->name('welcome');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::inertia('/dashboard', 'Dashboard')
+        ->name('dashboard');
+
+    Route::get('/list-users', [UserController::class, 'users'])
+        ->name('users');
+
+    Route::get('/profile', [UserController::class, 'profile'])
+        ->name('profile');
+
+    Route::post('/profile', [UserController::class, 'update'])
+        ->name('profile.update');
+
+    Route::delete('/profile/remove', [UserController::class, 'destroy'])
+        ->name('profile.delete');
 });
+
+require __DIR__ . '/auth.php';
