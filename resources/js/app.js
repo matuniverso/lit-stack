2f8d3b4fe56efaf9235391c4ab210a1a4783abd4
eumatv <miliandroxd@gmail.com>
2022-01-02 17:47:56 -0300

    basic assets setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-require('./bootstrap');
+import "./bootstrap"
+import React from "react"
+import { render } from "react-dom"
+import { createInertiaApp } from "@inertiajs/inertia-react"
+import { InertiaProgress } from "@inertiajs/progress"
+
+createInertiaApp({
+  title: (title) => `${title} - LIT Stack`,
+  resolve: (name) => require(`./Pages/${name}`),
+  setup({ el, App, props }) {
+    return render(<App {...props} />, el)
+  },
+})
+
+InertiaProgress.init({ color: "#4B5563" })
